f555a89b412810a7a1af5d5b569a60aeb43d3773
JuHwon <juer.wagner@gmail.com>
2016-09-12 19:05:36 +0200

    first draft

---
+++ README.md
@@ -0,0 +1,23 @@
+# tiss-registration-madness
+TISS Registration script
+
+
+
+## Requirements
+
+* Google Chrome with [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo "Tampermonkey"), or
+* Firefox with [Greasemonkey](https://addons.mozilla.org/de/firefox/addon/greasemonkey "Greasemonkey")
+
+## Getting started
+
+1. Download the UserScript and install it to Tampermonkey/Greasemonkey.
+2. Configure the script
+3. Visit your specific registration page.
+4. Enable Script
+5. Have fun.
+
+
+## TODO
+
+* Check if the correct course is selected
+* Test Exam and Group registration
---
+++ tiss-registration-madness.user.js
@@ -0,0 +1,278 @@
+// ==UserScript==
+// @name       TISS Registration Madness
+// @namespace  JuHwon
+// @version    1.0
+// @description  Just Madness
+// @match      https://tiss.tuwien.ac.at/*
+// @copyright  2016+, JuHwon
+// @require    http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
+
+(function TissRegistrationMadnessClass() {
+    var me = this;
+
+    var options = {
+        // option do enable autoregistration
+        autoRegistration: true,
+
+        // option to enable autoreload if the reigster button is not available
+        autoReload: false,
+
+        // interval to refresh the page if register button not available
+        refreshInterval: 1000,
+
+        // type of registration, available options: lva, group, exam
+        registrationType: 'lva',
+
+        // section of registration button, e.g. group name like "Gruppe 3", or name of exam
+        // this option is not available with the registration type 'lva'
+        registerSection: '',
+
+        // semester of the desired course
+        semester: '2016W',
+
+        // if you got multiple study codes, configure the desired one without the dot (e.g. 033526)
+        studyCode: ''
+    };
+
+    var tabs = {
+        lva: 'LVA-Anmeldung',
+        group: 'Gruppe',
+        exam: 'Prüfung'
+    };
+
+    var LOG_AREA_SYLE = 'color: black; background-color: #FFFCD9; font-size: 10pt;';
+    var LOG_AREA_ID = 'trmLog';
+    var ERROR_AREA_STYLE = 'color: red; font-weight: bold; font-size: 14pt; padding: 8px 0px;';
+    var ERROR_AREA_ID = 'trmError';
+    var SUCCESS_AREA_STYLE = 'color: green; font-weight: bold; font-size: 14pt; padding: 8px 0px;';
+    var SUCCESS_AREA_ID = 'trmSuccess';
+
+    this.init = function() {
+        me.extendJQuery();
+        me.injectArea(LOG_AREA_ID, LOG_AREA_SYLE, 'Information Log');
+        me.injectArea(ERROR_AREA_ID, ERROR_AREA_STYLE);
+        me.injectArea(SUCCESS_AREA_ID, SUCCESS_AREA_STYLE);
+        me.start();
+    };
+
+    this.start = function() {
+        me.pageLog('TISS Registration Madness started');
+        me.pageLog('LVA Number: ' + me.getLvaNumber());
+        me.pageLog('LVA Name: ' + me.getLvaName());
+        me.pageLog('SelectedTab: ' + me.getSelectedTab());
+
+        var properTabSelected = me.properTabSelected();
+        var studySelection = me.getStudySelection();
+        var confirmButton = me.getConfirmButton();
+        var alreadyRegistered = me.checkIfRegistered();
+
+        //TODO: check LVA number
+
+        if(!me.properSemesterSelected() || alreadyRegistered) {
+            return;
+        }
+
+        // check registration step
+        if (properTabSelected) {
+            me.register();
+        } else if (studySelection) {
+            me.studySelection(studySelection);
+        } else if (confirmButton) {
+            me.confirm(confirmButton);
+        } else {
+            me.pageError('Could not determine registration step.');
+        }
+    };
+
+    this.register = function() {
+        if (options.registrationType == 'lva') {
+            options.registerSection = 'LVA-Anmeldung';
+        }
+
+        var sectionLabel = this.getSectionLabel();
+        var section = sectionLabel.closest('.groupWrapper');
+        if (!section) {
+            pageError('Could not find section with title "' + options.registerSection + '"');
+            return;
+        }
+
+        me.highlightElement(sectionLabel);
+
+        var registerButton = me.getRegisterButton(section);
+        if (!registerButton) {
+            pageError('Could not find register button.');
+            if (options.autoReload) {
+                setTimeout(function() { location.reload(); }, options.refreshInterval);
+            }
+            return;
+        }
+        me.highlightElement(registerButton);
+
+        if (options.autoRegistration) {
+            registerButton.click();
+        }
+    };
+
+    this.studySelection = function(selection) {
+        me.pageError('');
+       if (options.studyCode) {
+            me.setSelectedValue(selection, options.studyCode);
+       }
+       var registerButton = me.getRegisterButton($('form#regForm'));
+       me.highlightElement(registerButton);
+       if (options.autoRegistration) {
+            registerButton.click();
+       }
+    };
+
+    this.confirm = function(button) {
+        me.pageError('');
+        me.highlightElement(button);
+        if (options.autoRegistration) {
+            button.click();
+        }
+    };
+
+    this.checkIfRegistered = function() {
+        if (options.registrationType == 'lva') {
+            options.registerSection = 'LVA-Anmeldung';
+        }
+
+        var sectionLabel = this.getSectionLabel();
+        var section = sectionLabel.closest('.groupWrapper');
+        if (!section) {
+            return false;
+        }
+
+        me.highlightElement(sectionLabel);
+
+        var deregisterButton = me.getDeregisterButton(section);
+        if (!deregisterButton) {
+            return false;
+        }
+        me.pageSuccess('You are successfully logged in.');
+        return true;
+    };
+
+///////////////////////// Props
+
+    this.getLvaNumber = function() {
+       return $('#contentInner h1 span:first').text().trim();
+    };
+
+    this.getLvaName = function() {
+        return $('#contentInner h1').justtext();
+    };
+
+    this.getSelectedTab = function() {
+        return $('li.ui-tabs-selected').text().trim();
+    };
+
+    this.getSectionLabel = function() {
+        return $(".groupWrapper .header_element span").filter(function () {
+            return $(this).text().trim() === options.registerSection;
+        });
+    };
+
+    // returns null if button is not found
+    this.getConfirmButton = function() {
+        var button = $("form#confirmForm input:submit[value='Ok']");
+        return button.length === 0 ? null : button;
+    };
+
+    // returns null if button is not found
+    this.getRegisterButton = function(section) {
+        var button = $(section).find('input:submit[value="Anmelden"]');
+
+        // fallback solutions, since it seems the value is not always the same
+        if (button.length === 0) {
+            button = $(section).find('input:submit[value="Voranmelden"]');
+        }
+
+        if (button.length === 0) {
+            button = $(section).find('input:submit[value="Voranmeldung"]');
+        }
+
+        return button.length === 0 ? null : button;
+    };
+
+    // returns null if button is not found
+    this.getStudySelection = function() {
+        var selection = $("#regForm").find('select[name="regForm:studyCode"]');
+        return selection.length === 0 ? null : selection;
+    };
+
+    // returns null if button is not found
+    this.getDeregisterButton = function(section) {
+        var button = $(section).find('input:submit[value="Abmelden"]');
+        return button.length === 0 ? null : button;
+    };
+
+//////////////////////// Util
+
+    this.highlightElement = function(element) {
+        element.css('background-color', 'lightgreen');
+    };
+
+    this.setSelectedValue = function (element, value) {
+        element.find('option').removeAttr('selected');
+        element.find('option[value="' + value + '"]').attr('selected', 'selected');
+    };
+
+    this.pageError = function(msg) {
+        var errorLog = $('#' + ERROR_AREA_ID);
+        errorLog.html(msg);
+    };
+
+    this.pageSuccess = function(msg) {
+        var successLog = $('#' + SUCCESS_AREA_ID);
+        successLog.html(msg);
+    };
+
+    this.pageLog = function(msg) {
+        var log = $('#' + LOG_AREA_ID);
+        log.html(log.html() + '<br />' + msg);
+    };
+
+    this.injectArea = function(id, style, title) {
+        $('#contentInner').before(
+            '<div id="' + id + '" style="' + style + '">' +
+                (title ? '<h2>' + title + '</h2>' : '') +
+            '</div>'
+        );
+    };
+
+    this.extendJQuery = function() {
+        jQuery.fn.justtext = function() {
+            //todo remove this in future
+            return $(this).clone()
+                .children()
+                .remove()
+                .end()
+                .text()
+                .trim();
+        };
+    };
+
+    //////////////////////// Validation
+
+    this.properSemesterSelected = function() {
+        var subheader = $('#contentInner #subHeader').text().trim();
+        if (subheader.indexOf(options.semester) === -1) {
+            me.pageError('Wrong semester selected: expected: ' + options.semester + ', got: ' + subheader.substring(0, 5));
+            return false;
+        }
+        return true;
+    };
+
+    this.properTabSelected = function() {
+        if (me.getSelectedTab() !== tabs[options.registrationType]) {
+            pageError('Wrong Tab selected. Tab did not match ' + tabs[options.registrationType]);
+            return false;
+        }
+        return true;
+    };
+
+    this.init();
+})();
